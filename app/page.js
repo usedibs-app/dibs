e6337d1dd2100e84aad68558b6f2980bc5d79d84
Ryanyzh <S10219317@connect.np.edu.sg>
2023-05-14 21:40:52 +0800

    created the shop and listing page

--- app/page.js
+++ app/page.js
@@ -1,14 +1,15 @@
 import Image from 'next/image'
-import LandingPage from './pages/HomePage.jsx'
-import ListingPage from './pages/ShopPage.jsx'
+import HomePage from './pages/HomePage.jsx'
+import ShopPage from './pages/ShopPage.jsx'
 import NavBar from './pages/NavSection.jsx'
 import Footer from './pages/FooterSection.jsx'
+import ListingPage from './pages/ListingPage.jsx'
 
 export default function Home() {
   return (
     <div className='w-screen max-w-screen h-screen px-16 py-6 bg-white'>
       <NavBar></NavBar>
-      <LandingPage></LandingPage>
+      <ListingPage></ListingPage>
       <Footer></Footer>
     </div>
   )
