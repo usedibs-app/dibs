744d6a4d5ff0fbc5950874ddec2484a7ee3c052a
Shuqri Shaifuddin <s10227932@connect.np.edu.sg>
2023-05-20 22:35:24 +0800

    gitignore whoops

--- app/login/page.js
+++ app/login/page.js
@@ -10,17 +10,18 @@ export default function Page() {
   const [password, setPassword] = useState("");
   const router = useRouter();
 
-  async function signInWithEmail() {
-    const { data, error } = await supabase.auth.signInWithPassword({
-      email: email,
-      password: password,
-    });
-    if (error) {
-      alert(error.message);
-      return;
-    }
-    else {
-      router.push("/");
+  async function signInWithEmail(e) {
+    e.preventDefault();
+    try {
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email: email,
+        password: password,
+      });
+      if (error) {
+        alert(error.message);
+        return;
+      }
+      alert("Check your email for the login link!");
     }
     // alert(JSON.stringify(data));
     // alert(JSON.stringify(error));
@@ -41,7 +42,10 @@ export default function Page() {
               <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                 Sign in to your account
               </h1>
-              <div class="space-y-4 md:space-y-6">
+              <form
+                class="space-y-4 md:space-y-6"
+                onSubmit={(e) => signInWithEmail(e)}
+              >
                 <div>
                   <label
                     for="email"
@@ -55,7 +59,7 @@ export default function Page() {
                     id="email"
                     class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="name@company.com"
-                    onChange={e => setEmail(e.target.value)}
+                    onChange={(e) => setEmail(e.target.value)}
                     required=""
                   />
                 </div>
@@ -105,7 +109,6 @@ export default function Page() {
                 </div>
                 <button
                   type="submit"
-                  onClick={signInWithEmail}
                   class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
                   Sign in
@@ -119,7 +122,7 @@ export default function Page() {
                     Sign up
                   </a>
                 </p>
-              </div>
+              </form>
             </div>
           </div>
         </div>
